b96f7171c218cc39feeeda2451add561616cf23f
Victor Santos <victor.santanasantos@ufpe.br>
2021-05-24 11:56:30 -0300

    Atividade da aula 06 sobre análise exploratória de dados

---
+++ Scripts/atividade_descoberta_aula_06.R
@@ -0,0 +1,12 @@
+# ATIVIDADE DESCOBERTA
+
+library(funModeling)
+library(tidyverse)
+
+# ANÁLISE EXPLORATÓRIA DOS DADOS
+
+glimpse(quakes)
+status(quakes)
+freq(quakes)
+plot_num(quakes)
+profiling_num(quakes)
